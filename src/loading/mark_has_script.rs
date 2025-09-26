5f0414a39f82267f2bfa06a9ad7e92882c00692f
someone1351 <someone1351@users.noreply.github.com>
2025-09-26 13:25:22 +1000

    some commit

--- src/loading/mark_has_script.rs
+++ src/loading/mark_has_script.rs
@@ -1,66 +1,37 @@
 
-use std::collections::HashMap;
 
 use super::vals::*;
 
-// pub fn mark_all_has_script(elements:&mut Vec<Element>) {
-//     let mut work_stk=vec![0];
-
-//     while let Some(cur_element_ind)=work_stk.pop() {
-//         let cur_element=elements.get(cur_element_ind).unwrap();
-
-//         if cur_element.use
-//         work_stk.extend(cur_element.children.iter().rev());
-
-
-//             let cur_element=elements.get_mut(cur_element_ind).unwrap();
-//             cur_element.has_script=true;
-//             cur_element.has_own_script=true;
-//             cur_element.has_apply_decl_script=true;
-//             cur_element.has_template_use_script=true;
-//     }
-// }
 
 pub fn mark_has_script(elements:&mut Vec<Element>,) {
-
-    // let mut element_parent_map: HashMap<usize, usize>=HashMap::new(); //[element]=parent
     let mut work_stk: Vec<usize>=vec![0];
-    let mut laters: Vec<usize>=Vec::new();
 
     while let Some(cur_element_ind)=work_stk.pop() {
         let cur_element=elements.get(cur_element_ind).unwrap();
 
         //
-        match &cur_element.element_type {
-            &ElementType::Apply { used, .. }|&ElementType::TemplateDecl { used, .. } if !used => {}
-            ElementType::TemplateUse {..}|ElementType::ApplyUse {..} => { //do later
-                laters.extend(cur_element.children.iter().rev());
-            }
-            _=>{
-                work_stk.extend(cur_element.children.iter().rev());
-                // element_parent_map.extend(cur_element.children.iter().map(|&child_element_ind|(child_element_ind, cur_element_ind)));
-            }
+        if cur_element.calcd_from_element_ind.is_some() {
+            panic!("mark_has_script, shouldn't be traversing generated elements");
         }
-        //
-        let has_self_script=match &cur_element.element_type {
-            ElementType::Script { .. } => true,
-            &ElementType::TemplateUse { template_decl_element_ind  } => { //template_decl will always be before template_use,
-                let element=elements.get(template_decl_element_ind).unwrap();
-                let &ElementType::TemplateDecl { used, .. }=&element.element_type else {panic!("");};
-                used && element.has_self_script
-            },
-            _ => false,
-        };
 
         //
-        let has_script=match &cur_element.element_type {
-            ElementType::Script { .. } => true,
+        match &cur_element.element_type {
+            &ElementType::Apply { used, .. }|&ElementType::TemplateDecl { used, .. } if !used => {}
+            ElementType::TemplateUse {..}|ElementType::ApplyUse {..} => {} //do children later
+            _=>{
+                work_stk.extend(cur_element.children.iter().rev());
+            }
+        }
+
+        //
+        let (has_script, has_self_script,)=match &cur_element.element_type {
+            ElementType::Script { .. } => (true,true),
             &ElementType::TemplateUse { template_decl_element_ind  } => { //template_decl will always be before template_use,
                 let element=elements.get(template_decl_element_ind).unwrap();
                 let &ElementType::TemplateDecl { used, .. }=&element.element_type else {panic!("");};
-                used && element.has_script
+                (used && element.has_script, used && element.has_self_script)
             },
-            _ => false,
+            _ => (false,false),
         };
 
         //set ancestors to has_script
@@ -69,28 +40,8 @@ pub fn mark_has_script(elements:&mut Vec<Element>,) {
 
             while let Some(element_ind2)=element_ind {
                 let element=elements.get_mut(element_ind2).unwrap();
-
                 element.has_script=true;
-
-
-                // match &element.element_type {
-                //     ElementType::TemplateUse {..} => {}
-                //     ElementType::Script {..} => {}
-                //     // ElementType::Node {..} if !first_node => {
-                //     //     first_node=true;
-                //     // }
-                //     // ElementType::Node {..}|ElementType::Apply {..}|ElementType::ApplyUse {..}|ElementType::TemplateDecl {..}|ElementType::Stub {..} => {bla=true;}
-                //     _ => {
-                //     }
-
-                // }
-                // if let ElementType::Apply {..}=&element.element_type {
-                //     bla=true;
-                // }
-
                 element_ind=element.parent;
-                // element_ind=element_parent_map.get(&element_ind2).cloned();
-
             }
         }
 
@@ -103,15 +54,7 @@ pub fn mark_has_script(elements:&mut Vec<Element>,) {
                 element.has_self_script=true;
 
                 match &element.element_type {
-                    ElementType::TemplateUse {..} => {}
-                    ElementType::Script {..} => {}
-                    // ElementType::Node {..} if !first_node => {
-                    //     first_node=true;
-                    // }
-                    //|ElementType::ApplyUse {..}
-                    //|ElementType::Stub {..} //wont reach it
-                    // ElementType::Node {..}|ElementType::Apply {..}|ElementType::TemplateDecl {..} => {
-                    // }
+                    ElementType::TemplateUse {..}|ElementType::Script {..} => {}
                     _ => {
                         break;
                     }
